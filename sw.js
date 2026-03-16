1f84c8d852d64b8b8c96d7fad7a7d7fa1b54508d
JELararagi <jelararagi@gmail.com>
2026-03-16 11:18:07 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = "v92-runtime-clean";
-const CACHE_NAME = 'trendbuzz-v84-cool-visual-lock';
+const CACHE_NAME = "meltdown-v93-v84base-drop-safe-collision";
 const PRECACHE = [
   './',
   './index.html',
